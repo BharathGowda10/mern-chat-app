6141cec5f5701c6b41582f30ade3870ee6506850
imbharathgowda10@gmail.com <imbharathgowda10@gmail.com>
2025-07-21 00:14:03 +0530

    for all paths adding route

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,8 +34,8 @@ if (process.env.NODE_ENV === "production") {
   console.log("Registering static file serving for production");
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  console.log("Registering wildcard route: *");
-  app.get("/*", (req, res) => {
+  console.log("Registering wildcard route: regex /^\\/.*/$");
+  app.get(/^\/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
